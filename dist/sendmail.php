127d7f4bfa7f1687b891cd2a2a5a5b1700c50a74
VeranikaM85 <105545886+VeranikaM85@users.noreply.github.com>
2023-02-26 21:07:30 +0300

    Add files via upload

---
+++ dist/sendmail.php
@@ -0,0 +1,47 @@
+<?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPMailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('postmaster@host200220231705.of.by', 'Форма отправки');
+	//Кому отправить
+	$mail->addAddress('manager@host200220231705.of.by');
+	//Тема письма
+	$mail->Subject = 'Привет!';
+
+	//Тело письма
+	$body = '<h1>Встречайте супер письмо!</h1>';
+	
+	if(trim(!empty($_POST['name']))){
+		$body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['social']))){
+		$body.='<p><strong>Phone:</strong> '.$_POST['social'].'</p>';
+	}
+
+
+	$mail->Body = $body;
+
+
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
